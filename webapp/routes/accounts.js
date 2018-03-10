506231da63a454e33d86a01c0209d71c0f6995a2
Peter MacNaughton <peter@macnaughton.info>
2018-03-10 10:58:20 -0700

    Implement database access in accounts route

--- webapp/routes/accounts.js
+++ webapp/routes/accounts.js
@@ -1,24 +1,40 @@
 const router = require('express').Router()
+const Account = require('../accountmodel')
 
-router.get('/', (req, res) => {
-    // TODO: make a call to database for accounts
-    return res.status(200).send([])
+router.get('/', (req, res, next) => {
+    Account.find({}, null, null, (error, accounts) => {
+        if(error) return next(error)
+        res.status(200).send(accounts)
+    })
 })
 
-router.post('/', (req, res) => {
-    let newAccount = req.body
-    // TODO: Store account in database
-    return res.status(201).send({id:newAccount.id})
+router.get('/:id', (req, res, next) => {
+    Account.findById(req.params.id, (error, account) => {
+        if(error) return next(error)
+        res.status(200).send(account)
+    })
 })
 
-router.put('/', (req, res) => {
-    // TODO: read account from database
-    let account = {}
-    account = Object.assign(account, req.body)
-    return res.status(200).send(account)
+router.post('/', (req, res, next) => {
+    let newAccount = new Account(req.body)
+    newAccount.save((error, account) => {
+        if(error) return next(error)
+        return res.status(201).send(account)
+    })
 })
 
-router.delete('/', (req, res) => {
-    // TODO: delete account from database
-    return res.status(200).send()
+router.put('/:id', (req, res, next) => {
+    Account.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, account) => {
+        if(error) next(error)
+        return res.status(200).send(account)
+    })
 })
+
+router.delete('/:id', (req, res, next) => {
+    Account.findByIdAndRemove(req.params.id, (error, account) => {
+        if(error) return next(error)
+        return res.status(200).send(account)
+    })
+})
+
+module.exports = router
